fcbbd07da8edbe7fc1c65222aa9bbf0e85dd2a5e
D1monik <116081418+D1monik@users.noreply.github.com>
2026-02-03 10:35:51 +0100

    kitoltottem a viewmodellt

--- WMSProject/EmployeeViewModell.cs
+++ WMSProject/EmployeeViewModell.cs
@@ -3,5 +3,7 @@
     public class EmployeeViewModell
     {
         public string Name { get; } = "Nagy Anna";
+        public string TaxNumber { get; } = "12345678-9-12";
+        public int DaysOff { get; } = 12;
     }
 }
